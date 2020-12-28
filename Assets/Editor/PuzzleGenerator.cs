6b1414966bd2144c27bde63d286676bd3b5a61c8
BabakFiroozi <bkfiroozi@gmail.com>
2020-12-28 20:40:14 +0330

    puzzle generator

--- Assets/Editor/PuzzleGenerator.cs
+++ Assets/Editor/PuzzleGenerator.cs
@@ -777,7 +777,7 @@ namespace Equation.Tools
         {
             bool NumberisPrime(int num)
             {
-                if (num == 1)
+                if (num < 4)
                     return false;
                 for(int i = 2; i < num; ++i)
                     if (num % i == 0)
